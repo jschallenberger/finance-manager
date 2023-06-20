ce112ca1893e8ee1cb717a6440e0cb6e9f0f4a71
Johann Schallenberger <schajohann@yahoo.com.br>
2023-06-20 20:18:14 -0300

    Table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,31 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
   return (
     <div>
       <Header/>
       <Summary/>
+
+    <TransactionsContainer>
+      <TransactionsTable>
+        <tbody>
+          <tr>
+            <td width="40%">Desenvolvimento do Site</td>
+            <td><PriceHighlight variant="income">R$ 12.000</PriceHighlight></td>
+            <td>Venda</td>
+            <td>13/05/2023</td>
+          </tr>
+          <tr>
+            <td width="40%">Alimentação</td>
+            <td><PriceHighlight variant="outcome">- R$ 12.000</PriceHighlight></td>
+            <td>Venda</td>
+            <td>13/05/2023</td>
+          </tr>
+        </tbody>
+      </TransactionsTable>
+    </TransactionsContainer>
     </div>
   )
 }
