981399c6d88aebedb997bc142368c2b0c50413d7
Johann Schallenberger <schajohann@yahoo.com.br>
2023-06-24 23:17:49 -0300

    Radix UI Dialog (Modal)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,30 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImage from "../../assets/logo.svg"
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header(){
   return (
     <HeaderContainer>
       <HeaderContent>
+
         <img src={logoImage} alt="Logo" />
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay/>
+            
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close/>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   )
